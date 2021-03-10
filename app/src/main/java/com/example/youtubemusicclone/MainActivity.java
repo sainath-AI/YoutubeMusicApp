1adabdde717a45c0b71a8cad22d4fceb5a633c7a
Mj-Khan <mjkhan7124@gmail.com>
2021-03-10 19:10:57 +0530

    upgrade frag

--- app/src/main/java/com/example/youtubemusicclone/MainActivity.java
+++ app/src/main/java/com/example/youtubemusicclone/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.youtubemusicclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentManager fragmentManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentManager =getSupportFragmentManager();
+        launchUpgradeFrag();
+    }
+
+    private void launchUpgradeFrag() {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        UpgradeFragment fragment = new UpgradeFragment();
+        transaction.add(R.id.flContainer, fragment,"upgradeFrag").commit();
     }
 }
