5bef09dbb25f56459bca4596785999dd840730a6
Sai Nath Reddy <dukedustine123@gmail.com>
2021-03-12 13:00:49 +0530

    update

--- app/src/main/java/com/example/youtubemusicclone/fragments/RelaxFragment.java
+++ app/src/main/java/com/example/youtubemusicclone/fragments/RelaxFragment.java
@@ -2,60 +2,41 @@ package com.example.youtubemusicclone.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.youtubemusicclone.GetLists.GetsLists;
 import com.example.youtubemusicclone.R;
+import com.example.youtubemusicclone.recyclers.WorkoutModel;
+import com.example.youtubemusicclone.recyclers.WorkoutViewHolderAdapterOne;
+import com.example.youtubemusicclone.recyclers.WorkoutViewHolderAdapterThree;
+import com.example.youtubemusicclone.recyclers.WorkoutViewHolderAdapterTwo;
+import com.example.youtubemusicclone.recyclers.WorkoutViewHolderAdapterfour;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RelaxFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
+
 public class RelaxFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView mRecyclerView1;
+    private RecyclerView mRecyclerView2;
+    private RecyclerView mRecyclerView3;
+    private RecyclerView mRecyclerView4;
 
-    public RelaxFragment() {
-        // Required empty public constructor
+    public RelaxFragment(){
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RelaxFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RelaxFragment newInstance(String param1, String param2) {
-        RelaxFragment fragment = new RelaxFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,4 +44,57 @@ public class RelaxFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_relax, container, false);
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initview(view);
+        buildRecyclerViewData_1();
+        buildRecyclerViewData_2();
+        buildRecyclerViewData_3();
+        buildRecyclerViewData_4();
+
+    }
+
+    private void buildRecyclerViewData_4() {
+        List<WorkoutModel> workoutModelList4= GetsLists.getWorkoutItemList4();
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
+        mRecyclerView4.setLayoutManager(linearLayoutManager);
+        WorkoutViewHolderAdapterfour workoutViewHolderAdapterfour= new WorkoutViewHolderAdapterfour(workoutModelList4);
+        mRecyclerView4.setAdapter(workoutViewHolderAdapterfour);
+    }
+
+    private void buildRecyclerViewData_3() {
+        List<WorkoutModel> workoutModelList3= GetsLists.getWorkoutItemList3();
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
+        mRecyclerView3.setLayoutManager(linearLayoutManager);
+        WorkoutViewHolderAdapterThree workoutViewHolderAdapterThree= new WorkoutViewHolderAdapterThree(workoutModelList3);
+        mRecyclerView3.setAdapter(workoutViewHolderAdapterThree);
+    }
+
+    private void buildRecyclerViewData_2() {
+        List<WorkoutModel> workoutModelList2= GetsLists.getWorkoutItemList2();
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
+        mRecyclerView2.setLayoutManager(linearLayoutManager);
+        WorkoutViewHolderAdapterTwo workoutViewHolderAdapterTwo= new WorkoutViewHolderAdapterTwo(workoutModelList2);
+        mRecyclerView2.setAdapter(workoutViewHolderAdapterTwo);
+    }
+
+    private void buildRecyclerViewData_1() {
+        List<WorkoutModel> workoutModelList1= GetsLists.getWorkoutItemList1();
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
+        mRecyclerView1.setLayoutManager(linearLayoutManager);
+        WorkoutViewHolderAdapterOne workoutViewHolderAdapterOne= new WorkoutViewHolderAdapterOne(workoutModelList1);
+        mRecyclerView1.setAdapter(workoutViewHolderAdapterOne);
+    }
+
+    private void initview(View view) {
+        mRecyclerView1 = view.findViewById(R.id.recyclerView_1);
+        mRecyclerView2 = view.findViewById(R.id.recyclerView_2);
+        mRecyclerView3 = view.findViewById(R.id.recyclerView_3);
+        mRecyclerView4 = view.findViewById(R.id.recyclerView_4);
+    }
+
+
 }
